13f8b457aa9d335fa7be1caca2427ed0935606f3
mishra-lucky <83387735+mishra-lucky@users.noreply.github.com>
2021-08-28 11:59:28 +0530

    Added new algo

---
+++ FloydWarshallsApSp/FloydWarshallsApSp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydWarshallsApSp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] graph = { new int[]{ 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                                     new int[] { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                                      new int[]{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                                      new int[]{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                                     new int[] { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                                     new int[] { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                                      new int[]{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                                     new int[] { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                                      new int[]{ 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+            };
+            int[,] dist = GetAllPairShortestPath(graph);
+            for (int i = 0; i < graph.Length; i++)
+                for (int j = 0; j < graph[0].Length; j++)
+                    Console.WriteLine(string.Format("Distance between {0} and {1}  is {2}.", i, j, dist[i, j]));
+                             
+            Console.ReadLine();
+        }
+
+        private static int[,] GetAllPairShortestPath(int[][] graph)
+        {
+            int[,] dp = new int[graph.Length, graph[0].Length];
+            int[,] next = new int[graph.Length, graph[0].Length];
+
+            for(int i=0; i<graph.Length;i++)
+            {
+                for(int j=0; j< graph[0].Length; j++)
+                {
+                    dp[i, j] = graph[i][j];
+                    next[i, j] = j;
+                    if (graph[i][j] == 0)
+                        dp[i, j] = int.MaxValue;
+                    if (i == j)
+                        dp[i, j] = 0;
+                }
+            }
+
+            // fill dp
+            for(int k=0; k < graph.Length; k++)
+            {
+                for(int i=0; i<graph.Length; i++)
+                {
+                    for(int j =0; j < graph[0].Length; j++)
+                    {
+                        if(dp[i,k] != int.MaxValue && dp[k,j] != int.MaxValue && dp[i,k] + dp[k,j] < dp[i,j])
+                        {
+                            dp[i, j] = dp[i, k] + dp[k, j];
+                            next[i, j] = next[i,k];
+                        }
+                    }
+                }
+            }
+
+            // negetive cycles
+            for (int k = 0; k < graph.Length; k++)
+            {
+                for (int i = 0; i < graph.Length; i++)
+                {
+                    for (int j = 0; j < graph[0].Length; j++)
+                    {
+                        if (dp[i, k] != int.MaxValue && dp[k, j] != int.MaxValue  && dp[i, k] + dp[k, j] < dp[i, j])
+                        {
+                            dp[i, j] = int.MinValue;
+                            next[i, j] = -1;
+                        }
+                    }
+                }
+            }
+            return dp;
+            
+        }
+    }
+}
---
+++ PallindromePermutationsII/PallindromePermutationsII/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallindromePermutationsII
+{
+    class Program
+    {
+        HashSet<string> results = new HashSet<string>();
+        int strlen = 0;
+        static void Main(string[] args)
+        {
+            string s1 = "aaa";
+            Program p1 = new Program();
+            List<string> l = p1.generateAllPallindromicPermutations(s1);
+            Console.WriteLine(string.Join(",", l));
+            Console.ReadLine();
+        }
+
+        private List<string> generateAllPallindromicPermutations(string s)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+            foreach (char c in s)
+                if (freq.ContainsKey(c))
+                    freq[c]++;
+                else
+                    freq.Add(c, 1);
+
+            int distinctchars = freq.Keys.Count;
+            char[] chars = new char[distinctchars];
+            int[] charcount = new int[distinctchars];
+            int idx = 0;
+            foreach (var kvp in freq)
+            {
+                chars[idx] = kvp.Key;
+                charcount[idx++] = kvp.Value;
+                strlen += kvp.Value;
+            }
+            char[] buffer = Enumerable.Repeat('-', strlen).ToArray();
+            Permutate(buffer, chars, charcount, 0, 0);
+            return results.ToList();
+        }
+
+        public void Permutate(char[] buffer, char[] chars, int[] charcount, int bufferLength, int buffindex)
+        {
+            if (bufferLength == strlen)
+            {
+                string added = new string(buffer);
+                if (!results.Contains(added))
+                    results.Add(added);
+                Console.WriteLine(bufferLength);
+                return;
+            }
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (charcount[i] == 0)
+                    continue;
+                if (charcount[i] >= 2 && buffer[buffindex] == '-')
+                {
+                    buffer[buffindex] = chars[i];
+                    buffer[buffer.Length - 1 - buffindex] = chars[i];
+                    charcount[i] -= 2;
+                    Permutate(buffer, chars, charcount, bufferLength + 2, buffindex + 1);
+                    charcount[i] += 2;
+                    buffer[buffer.Length - 1 - buffindex] = '-';
+                    buffer[buffindex] = '-';
+                }
+                else if (buffer[buffindex] == '-' && buffindex == Math.Floor((decimal)strlen / 2))
+                {
+                    buffer[buffindex] = chars[i];
+                    charcount[i] -= 1;
+                    Permutate(buffer, chars, charcount, bufferLength + 1, buffindex + 1);
+                    charcount[i] += 1;
+                    buffer[buffindex] = '-';
+                }
+
+            }
+        }
+    }
+}
